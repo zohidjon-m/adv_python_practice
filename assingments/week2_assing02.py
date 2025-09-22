9e39e7a7247b95202e7f4fde8c657ad6b3431fad
zohidjon-m <148512253+zohidjon-m@users.noreply.github.com>
2025-09-22 15:31:15 +0900

    upload assingments

---
+++ assingments/week2_assing02.py
@@ -0,0 +1,7 @@
+
+#assignment 02
+int_v = int(input("Enter a int value: "))
+float_v = float(input("Enter a float value: "))
+
+sum_v = int_v+float_v
+print(f"result{sum_v}")
---
+++ assingments/week2_assing03.py
@@ -0,0 +1,8 @@
+
+#assignment 3
+
+name  = str(input("enter your name: "))
+age = int(input("enter your age: "))
+gender = int(input("enter you gender: "))
+height = float(input("enter your height: "))
+print(f"user's name is {name}, her age is {age}, gender is {gender}, height is {height}")
